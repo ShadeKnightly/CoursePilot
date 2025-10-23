814b42faa9fdb8972c9eeb15b0ba006f6a8908ee
BLafleur <BDLAFLEUR21@gmail.com>
2025-10-23 14:36:52 -0600

    background component finished

--- course-pilot/src/App.js
+++ course-pilot/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Background from "./components/Background/Background";
 import "./styles/Variables.css";
 
 function App() {
   return (
     <Router>
+      <Background>
       <Header />
       <Routes>
         <Route path="/" element={null} />
       </Routes>
+      </Background>
     </Router>
   );
 }
