ec649490754ef0a4eb52f0400a5d987ab5649a06
BLafleur <BDLAFLEUR21@gmail.com>
2025-10-29 10:36:41 -0600

    Made course-Registration page
    updated card component to have search and cart icons

--- course-pilot/src/components/card/cardComponent.jsx
+++ course-pilot/src/components/card/cardComponent.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
 import "./cardComp.css";
+import { Link, useLocation } from "react-router-dom";
 
-function CardComp({ title, children }) {
+const CardComp = ({ title, children }) => {
+  const location = useLocation();
+
+  // Only show icons on the Course Registration page
+  const showIcons = location.pathname === "/registration";
+
   return (
     <div className="card">
-      <div className="card-header">{title}</div>
+      <div className="card-header">
+        <h2 className="card-title">{title}</h2>
+        {showIcons && (
+          <div className="card-icons">
+            <Link to="/search" className="icon-button">
+              <FaSearch />
+            </Link>
+            <Link to="/cart" className="icon-button">
+              <FaShoppingCart />
+            </Link>
+          </div>
+        )}
+      </div>
       <div className="card-body">{children}</div>
     </div>
   );
-}
+};
 
 export default CardComp;
