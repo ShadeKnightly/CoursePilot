438c59743633022b74a9fa397b5613e514469359
Braydon Lafleur-Brown <BDLAFLEUR21@gmail.com>
2025-12-12 16:03:12 -0700

    Remove debug log for currentUser
    Removed debug console log for currentUser.

--- course-pilot/src/pages/Student/contactPage/contactPage.jsx
+++ course-pilot/src/pages/Student/contactPage/contactPage.jsx
@@ -12,9 +12,6 @@ const Contact = () => {
   });
   const { currentUser } = useContext(UserContext);
 
-  //debug
-  console.log("currentUser:", currentUser);
-
 
   const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
